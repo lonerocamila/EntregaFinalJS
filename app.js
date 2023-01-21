7c6dd4b856e9ab0a444c31e989fbc8c43af5dade
Camila Lo Nero <lonerocamila@gmail.com>
2023-01-21 18:42:59 -0300

    introducir productos en landing

--- app.js
+++ app.js
@@ -9,26 +9,32 @@ fetch("data.json") // Hacemos la petición al archivo data.json
         // Guardamos los datos en una variable
        const productos = data;   
         // Llamamos a la función que muestra los productos
-        function mostrarProductos() {
-        productos.forEach(producto => {
-            let content = document.createElement("div");
-            // Para crear etiquetas HTML dentro del div que creamos usamos innerHTML
-            content.innerHTML = `
-               <img src="{$producto.img}>
-               <h3>${producto.nombre}</h3>
-               p>${producto.descripcion}</p>
-               <p>${producto.precio} $ </p>
-               <a href="carrito.html" class="btn btn-primary">Agregar al carrito</a>
-            
-            
-            `
-            shopContent.appendchild(div);
-            shopContent.append(content);
+        
 
 
-        });
+        }); 
     
 
-    }
-    });
+        function mostrarProductos() {
+            productos.forEach(producto   => {
+                let content = document.createElement("div");
+                // Para crear etiquetas HTML dentro del div que creamos usamos innerHTML
+                content.innerHTML = `
+                   <img src="{$producto.img}">
+                   <h3>${producto.nombre}</h3>
+                   p>${producto.descripcion}</p>
+                   <p>${producto.precio} $ </p>
+                   <a href="carrito.html" class="btn btn-primary">Agregar al carrito</a>
+                `;
+                shopContent.append(content);
 
+                let comprar = document.getElementsByClassName("btn btn-primary");
+                comprar.innerText = "Agregar al carrito";
+                comprar.addEventListener("click", () => {
+                    carrito.push(producto);
+                    content.appendChild(comprar);
+                });
+            });     
+        }
+
+            
