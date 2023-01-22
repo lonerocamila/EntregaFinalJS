d15c6d599ee6d817a89123e354a30dfe789730b3
Camila Lo Nero <lonerocamila@gmail.com>
2023-01-22 11:41:42 -0300

    arreglos

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const shopContent = document.getElementById("shopContent");
 
+const verCarrito  = document.getElementById("verCarrito");
+const productoLS = localStorage.getItem("carrito");
+producto = document.getElementById("producto");
 
 let carrito = [];
 
@@ -7,34 +9,73 @@ fetch("data.json") // Hacemos la petición al archivo data.json
     .then(response => response.json()) // Convertimos la respuesta a JSON
     .then(data => {
         // Guardamos los datos en una variable
-       const productos = data;   
+        const productos = data;
         // Llamamos a la función que muestra los productos
+        mostrarProductos(productos) // llamar a la función y que reciba productos
+    });
+
+
+function mostrarProductos(productos) { // agregar el parámetro productos.
+const mostrarProductos = JSON.stringify(producto);
+                            mostrarProductos.forEach((producto) => {
+                                let content = document.createElement("div");
+                                content.className = "card";
+                                content.className = "card";
+                                content.innerHTML = `
+                                <img src="{$producto.img}">
+                                <h3 class="nombre">${producto.nombre}</h3>
+                                <p class="descripcion">${producto.descripcion}</p>
+                                <p class="precio">${producto.precio} $ </p>
+                                <a href="carrito.html" class="btn btn-primary">Agregar al carrito</a>
+                        
+                        `;
+                mostrarProductos.append(content);
+
+                let comprar = document.createElement("button");
+                comprar.innerText = "comprar";
+                comprar.className="comprar"; 
+               
+                content.append(comprar);
+                comprar.addEventListener("click", () => { 
+                    carrito.push({
+                       
+                    
+                        id: producto.id,
+                        img: producto.img,
+                        nombre: producto.nombre,
+                        precio: producto.precio,
+                        cantidad: 1,    
+                    });
+
+                });
+            });
+
+           
+
+
+    
+verCarrito.addEventListener("click", () => {
+    carrito.forEach((producto) => {
+        let carritoContent = document.createElement("div");
+        carrito.innerHTML  = `
+        <img src="${producto.img}">
+        <h3 class="nombre">${producto.nombre}</h3>
+        <p class="precio">${producto.precio} $ </p>
+        <p class="cantidad">${producto.cantidad}</p>
+        `;
+        modalContainer.append(carritoContent);
         
 
+});
+    const total = carrito.reduce((acc, el)=> acc + el.precio, 0);
+    const totalComprado = document.createElement("div");
+    totalComprado.className = "total-content"
+    totalComprado.innerHTML = ` total: ${total} $`
+    modalContainer.append(totalComprado);
+});
 
-        }); 
-    
 
-        function mostrarProductos() {
-            productos.forEach(producto   => {
-                let content = document.createElement("div");
-                // Para crear etiquetas HTML dentro del div que creamos usamos innerHTML
-                content.innerHTML = `
-                   <img src="{$producto.img}">
-                   <h3>${producto.nombre}</h3>
-                   p>${producto.descripcion}</p>
-                   <p>${producto.precio} $ </p>
-                   <a href="carrito.html" class="btn btn-primary">Agregar al carrito</a>
-                `;
-                shopContent.append(content);
+                        }))
+                    }
 
-                let comprar = document.getElementsByClassName("btn btn-primary");
-                comprar.innerText = "Agregar al carrito";
-                comprar.addEventListener("click", () => {
-                    carrito.push(producto);
-                    content.appendChild(comprar);
-                });
-            });     
-        }
-
-            
+// FILTRO DE BUSQUEDA
