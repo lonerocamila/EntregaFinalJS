fef625bb9b2e6bbfb43c68c36a53df7ef7305034
Camila Lo Nero <lonerocamila@gmail.com>
2023-02-01 19:07:42 -0300

    separacion de las funciones y cambio de ruta.json

--- js/app.js
+++ js/app.js
@@ -1,37 +1,36 @@
 const contenedorCards = document.getElementById("contenedorCards")
-const verCarrito= document.getElementById("verCarrito")
-const modalContainer= document.getElementById("modal-container")
-const cantidadCarrito =document.getElementById("cantidadCarrito")
+const verCarrito = document.getElementById("verCarrito")
+const modalContainer = document.getElementById("modal-container")
+const cantidadCarrito = document.getElementById("cantidadCarrito")
 const producto = document.getElementById("producto")
-const vaciarCarrito = document.getElementById("vaciarCarrito");
-const carrito = document.getElementById("carrito");
 
 
 // Traemos los datos del JSON
-const fetchData = async () => {
-	fetch("./data.js")
-		.then((response) => response.json())
-		.then((productos) => {
-            pintarCards(productos);
-        });
-};
+    const fetchData = async () => {
+        fetch("../data.json")
+            .then((response) => response.json())
+            .then((productos) => {
+                pintarCards(productos);
+            });
 
-// Funcion para pintar los productos
-const pintarCards = (productos) => {
-    // Creamos un array para guardar los productos del carrito
-    let carrito =JSON.parse(localStorage.getItem("carrito")) || [];
-// Funcion para guardar el carrito en el local storage
-     const GuardarLocal = () => {
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-};
-// Funcion para controlar la cantidad de productos en el carrito
-const carritoCounter = () => {
-    let cantidad = 0;
-    carrito.forEach((producto) => {
-        cantidad += producto.cantidad;
+    };
+
+ // Creamos un array para guardar los productos del carrito
+ function carrito () {
+    document.getElementById("carrito").innerHTML = localStorage.getItem("carrito");
+    document.getElementById("carrito").addEventListener("click", function () {
+        localStorage.setItem("carrito", JSON.stringify(carrito));
     });
-    cantidadCarrito.innerText = cantidad;
-};
+   carrito = [];
+}   
+
+  
+
+// Funcion para mostrar el carrito
+function mostrarCarrito() {
+    modalContainer.classList.toggle("show")
+}
+
 // Funcion para agregar productos al carrito
 const agregarCarrito = (e) => {
     if (e.target.classList.contains("btn")) {
@@ -79,12 +78,21 @@ const agregarCarrito = (e) => {
         carritoCounter();
     }
 };
+// Funcion para controlar la cantidad de productos en el carrito
+const carritoCounter = () => {
+    let cantidad = 0;
+    carrito.forEach((producto) => {
+        cantidad += producto.cantidad;
+    });
+    cantidadCarrito.innerText = cantidad;
+};
+
 // Funcion para pintar el carrito
 const pintarCarrito = () => {
     // limpiamos el html
-    items.innerHTML = "";
+    contenedorCards.innerHTML = "";
     // recorremos el carrito
-    carrito.forEach((producto) => {
+    modalContainer.forEach((producto) => {
         // creamos el html
         const div = document.createElement("div");
         div.className = "card";
@@ -92,9 +100,19 @@ const pintarCarrito = () => {
         <img src="${producto.img}" class="card-img-top">
         <div class="card-body">
         <h5 class="card-title">${producto.nombre}</h5>
-        <p class="card-text">${producto.peso} $${producto.precio}</p>
+         $${producto.precio}</p>
         </div>
         `;
+        // creamos el boton
+        const btn = document.createElement("button");
+        btn.className = "btn btn-success";
+        btn.innerText = "Comprar";
+        btn.addEventListener("click", agregarCarrito);
+    });
+};
+function carritoActions(){
+ document.getElementById("vaciarCarrito").addEventListener("click", vaciarCarrito);
+
             // usamos el metodo filter para ELIMINAR el producto del carrito
             vaciarCarrito = contenedorCards.filter((producto) => producto.id !== producto.id);
              // escuchamos el evento click del boton eliminar
@@ -124,20 +142,56 @@ const pintarCarrito = () => {
             GuardarLocal();
             // controlamos la cantidad de productos en el carrito
             carritoCounter();
+            document.getElementById("vaciarCarrito").addEventListener("click", vaciarCarrito);
+            });
+        }
+        
 
-        });
-    });
-};
-// Funcion para controlar los botones del carrito
-const btn = (e) => {
-    // aumentar
-    if (e.target.classList.contains("btn-success")) {
-        // usamos el metodo map para AUMENTAR la cantidad del producto en el carrito
-        carrito.map((producto) => {
-            if (producto.id === producto.id) {
-                producto.cantidad++;
-            }
-        });
+            // usamos el metodo FILTER para ELIMINAR el producto del carrito
+           function vaciarCarrito () {
+            
+            vaciarCarrito = contenedorCards.filter((producto) => producto.id !== producto.id);
+             // escuchamos el evento click del boton eliminar
+            vaciarCarrito.addEventListener("click", ()=>{
+                Toastify({
+                    text: "Producto eliminado",
+                    duration: 3000,
+                    newWindow: true,
+                    close: true,
+                    gravity: "top", 
+                    position: "right", 
+                    stopOnFocus: true, 
+                    style: {
+                    background: "grey",
+                    },
+                    onClick: function(){} 
+                }).showToast();
+                 vaciarCarrito(producto.id);
+
+            // agregamos el boton al html
+            div.append(btn);
+            // agregamos el html al contenedor
+            items.append(div);
+            // pintamos el carrito
+            pintarCarrito();
+            // guardamos el carrito en el local storage
+            GuardarLocal();
+            // controlamos la cantidad de productos en el carrito
+            carritoCounter();
+            document.getElementById("vaciarCarrito").addEventListener("click", vaciarCarrito);
+            });
+        }
+
+        // Funcion para controlar los botones del carrito
+        const btn = (e) => {
+            // aumentar
+            if (e.target.classList.contains("btn-success")) {
+                // usamos el metodo map para AUMENTAR la cantidad del producto en el carrito
+                carrito.map((producto) => {
+                    if (producto.id === producto.id) {
+                        producto.cantidad++;
+                    }
+                });
         Toastify({
             text: "Producto agregado al carrito",
             duration: 3000,
@@ -157,7 +211,7 @@ const btn = (e) => {
         GuardarLocal();
         // controlamos la cantidad de productos en el carrito
         carritoCounter();
-    }   
+    
     // disminuir
     if (e.target.classList.contains("btn-warning")) {
         // usamos el metodo map para DISMINUIR la cantidad del producto en el carrito
@@ -185,18 +239,11 @@ const btn = (e) => {
         GuardarLocal();
         // controlamos la cantidad de productos en el carrito
         carritoCounter();
-    }
     
+        };
+    }
 
-// Esperamos que cargue todo el contenido del DOM y pintamos carrito en caso de que exista informacion
 
-document.addEventListener("DOMContentLoaded", () => {
-    fetchData();
-    if (localStorage.getItem("carrito")) {
-        carrito = JSON.parse(localStorage.getItem("carrito"))
-        pintarCarrito()     
-    } 
-})
 
 // Creamos los eventos
 
@@ -210,9 +257,9 @@ items.addEventListener("click", (e) => {
 
 // Funcion para agregar productos al carrito
         // pintamos el carrito en caso de que exista informacion
-			productos.forEach((producto) => {
+			producto.forEach((producto) => {
 				let content = document.createElement("div");
-				content.className = "card";
+				modalContainer.className = "card";
 				content.innerHTML = `
         <img src="${producto.img}" class="card-img-top">
         <div class="card-body">
@@ -225,7 +272,7 @@ items.addEventListener("click", (e) => {
         contenedorCards.appendChild(content);
         
     //  boton de compra 
-        let comprar = document.createElement("button");
+        let comprar = document.createElement("buttonComprar");
         comprar.innerText = "comprar";
         comprar.className = "btn btn-primary";
 
@@ -317,7 +364,7 @@ items.addEventListener("click", (e) => {
             }
           
 
-        } 
+        
 
         // escuchamos el evento click del boton enviar 
         function sendbutton (){
